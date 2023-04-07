1a1b0c9255b2ea768dcda247e540d9744ca15b59
sina <sina_od@yahoo.com>
2023-04-07 13:58:11 +0330

    Added  module

---
+++ RLottieBinding/RLottieBinding/RLottieBinding.h
@@ -0,0 +1,18 @@
+//
+//  RLottieBinding.h
+//  RLottieBinding
+//
+//  Created by Sina on 4/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RLottieBinding.
+FOUNDATION_EXPORT double RLottieBindingVersionNumber;
+
+//! Project version string for RLottieBinding.
+FOUNDATION_EXPORT const unsigned char RLottieBindingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RLottieBinding/PublicHeader.h>
+
+#import "LottieInstance.h"
---
+++ UIComponents/UIComponents/Sources/AnimatedSticker/WAnimatedSticker.swift
@@ -0,0 +1,13 @@
+//
+//  WAnimatedSticker.swift
+//  UIComponents
+//
+//  Created by Sina on 4/7/23.
+//
+
+import UIKit
+import RLottieBinding
+
+@IBDesignable
+class WAnimatedSticker: UIView {
+}
