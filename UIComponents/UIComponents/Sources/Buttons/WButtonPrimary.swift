5b40b3bd6401e1dbe33f4c40fface4cd7777ed26
sina <sina_od@yahoo.com>
2023-04-07 16:08:05 +0330

    - set min iOS version to 12 - fix `UIButton` issues

--- UIComponents/UIComponents/Sources/Buttons/WButtonPrimary.swift
+++ UIComponents/UIComponents/Sources/Buttons/WButtonPrimary.swift
@@ -14,20 +14,28 @@ fileprivate let _font = UIFont.systemFont(ofSize: 17, weight: .semibold)
 @IBDesignable
 class WButtonPrimary: UIButton {
 
-    override open func awakeFromNib() {
+    override func awakeFromNib() {
         super.awakeFromNib()
         setup()
     }
     
-    override open func prepareForInterfaceBuilder() {
+    override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
-        setup()
+        setup(interfaceBuilder: true)
     }
     
-    func setup() {
-        // set colors
+    func setup(interfaceBuilder: Bool = false) {
+        // disable default styling of iOS 15+ to prevent tint/font set conflict issues
+        if #available(iOS 15.0, *) {
+            // setting configuration to .none on interface builder makes text disappear
+            if !interfaceBuilder {
+                configuration = .none
+            }
+        }
+        // set background color
         backgroundColor = .systemBlue
-        titleLabel?.textColor = UIColor.white
+        // set title color
+        tintColor = .white
         // set corner radius
         layer.cornerRadius = _borderRadius
         // set height anchor as default value
